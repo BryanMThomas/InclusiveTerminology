d4847bbb8803fc0f652e4a9e8bcfec75859f8252
Bryan Thomas <bryanthomas@microsoft.com>
2022-04-24 15:43:30 -0700

    update taskpane

---
+++ outlook-extension/src/taskpane/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Content } from "./Content";
+
+function App() {
+  //Currently a wrapper for Content Component
+  return (
+    <React.Fragment>
+      <Content />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ outlook-extension/src/taskpane/components/Results/Results.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Result } from "./Result/Result";
+
+
+export const Results = (props) => {
+  if (props.termsData === null) {
+    return (
+      <div>
+        <h3>No issues found with this email. Nice job!</h3>
+      </div>
+    );
+  } else if (
+    props.termsData === "emptyBody" ||
+    props.termsData.length === undefined
+  ) {
+    //initial load or searched for empty string
+    return null;
+  } else {
+    return (
+      <div>
+        <h3>Found Non-Inclusive Terms</h3>
+        {props.termsData.map((element, index) => (
+          <Result result={element} key={`result-${index}`} />
+        ))}
+      </div>
+    );
+  }
+};
